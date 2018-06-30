149a063ec3c424d97e3f075482ef7ca05dabb33b
pythonCat <pythoncat@qq.com>
2018-06-30 11:14:43 +0800

    add a localBroadcatReceiver

--- app/src/main/java/com/python/cat/restartandroid/MainActivity.java
+++ app/src/main/java/com/python/cat/restartandroid/MainActivity.java
@@ -1,42 +1,65 @@
 package com.python.cat.restartandroid;
 
 import android.app.Notification;
-import android.app.NotificationChannel;
 import android.app.NotificationManager;
-import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Color;
-import android.net.Uri;
-import android.os.Build;
-import android.support.v4.app.NotificationCompat;
+import android.content.IntentFilter;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
+import com.apkfuns.logutils.Constant;
 import com.apkfuns.logutils.LogUtils;
 import com.python.cat.restartandroid.services.BackendService;
+import com.python.cat.restartandroid.utils.Common;
 import com.python.cat.restartandroid.utils.NotificationTools;
-import com.python.cat.restartandroid.utils.NotificationUtils;
-
-import java.nio.channels.Channel;
 
 public class MainActivity extends AppCompatActivity {
 
     private NotificationManager mNotificationManager;
+
     private String channelId;
+    private InnerBroadcastReceiver receiver;
+    private IntentFilter intentFilter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        mNotificationManager = (NotificationManager)
+                getSystemService(Context.NOTIFICATION_SERVICE);
+
+        receiver = new InnerBroadcastReceiver();
+        intentFilter = new IntentFilter();
+        intentFilter.addAction(Common.INNERACTION);
+        LocalBroadcastManager.getInstance(this)
+                .registerReceiver(receiver, intentFilter);
+
         View btnSendNotification = findViewById(R.id.show_notification);
         View btnStartService = findViewById(R.id.start_service);
-        clickView(btnSendNotification);
+        View btnSendBroadcast = findViewById(R.id.send_broadcast);
+        click2sendNotification(btnSendNotification);
+        click2startService(btnStartService);
+        click2sendBroadcast(btnSendBroadcast);
 
+    }
 
+    private void click2sendBroadcast(View btnSendBroadcast) {
+        btnSendBroadcast.setOnClickListener(v -> {
+            LogUtils.w("click 2 send broadcast");
+            Intent intent = new Intent();
+            intent.setAction(Common.INNERACTION);
+            intent.putExtra("aaa", "bbb");
+            LocalBroadcastManager.getInstance(this)
+                    .sendBroadcast(intent);
+        });
+    }
+
+    private void click2startService(View btnStartService) {
         btnStartService
                 .setOnClickListener(v -> {
                             LogUtils.w("click 2 start service...");
@@ -44,10 +67,9 @@ public class MainActivity extends AppCompatActivity {
                                     BackendService.class));
                         }
                 );
-
     }
 
-    private void clickView(View viewById) {
+    private void click2sendNotification(View viewById) {
         Notification notification = NotificationTools
                 .builder(mNotificationManager, this.getApplicationContext())
                 .build();
@@ -65,5 +87,22 @@ public class MainActivity extends AppCompatActivity {
             mNotificationManager.deleteNotificationChannel(channelId);
         }
         mNotificationManager.cancelAll();
+        if (receiver != null) {
+            LocalBroadcastManager.getInstance(this).unregisterReceiver(receiver);
+        }
+    }
+
+
+    private class InnerBroadcastReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            LogUtils.w("inner receive.." + context + " , " + intent);
+            String action = intent.getAction();
+            LogUtils.w("receive action: " + action);
+
+            Toast.makeText(MainActivity.this,
+                    "收到广播了...",Toast.LENGTH_SHORT).show();
+        }
     }
 }
---
+++ app/src/main/java/com/python/cat/restartandroid/utils/Common.java
@@ -0,0 +1,9 @@
+package com.python.cat.restartandroid.utils;
+
+public class Common {
+
+    private Common() {
+    }
+
+    public static final String INNERACTION = "com.python.cat.restartandroid.innerbroadcast";
+}
